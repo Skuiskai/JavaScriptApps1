99ba94933cf34c7edafe7edb3ff2a8ecb0090412
Skuiskai <skuiskaiofficial@gmail.com>
2019-05-09 18:51:16 -0400

    index.html

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //what number would you like to count to?
 //By what number would you like to count?
 //Count By X App
+//comments
 function countTo () {
 	//first get a max number from the user
 	let countMax = Number(prompt("What number would you like to count to?"));
